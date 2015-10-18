c476b3105c5ff1d6a8a1c34a32061a01ddefced5
Panu Horsmalahti <panu.horsmalahti@iki.fi>
2015-10-18 19:38:32 +0300

    Add express.js example.

---
+++ example/app.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+app.use('/', express.static(__dirname));
+
+app.listen(port);
+console.log("Listening http://127.0.0.1:" + port);
